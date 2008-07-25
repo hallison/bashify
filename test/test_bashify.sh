03477e2dbccbe0d20d8463a7d4b866aa352aecc9
Hallison Vasconcelos Batista <hallison.batista@gmail.com>
2008-07-25 00:50:05 -0400

    Added functions to make directory tree for new projects.

--- test/test_bashify.sh
+++ test/test_bashify.sh
@@ -1,4 +1,42 @@
-function test_should_build_new_project() {
-  bin/bashify new foo
 
+declare -F test_should_build_new_project
+declare -F test_should_validates_directory_tree
+
+declare project_path="test/bashfoo"
+
+function test_should_build_new_project {
+  bin/bashify $project_path
+  [[ $? ]] && echo "R: Success" || "> Failure"
 }
+
+function test_should_validates_directory_tree {
+  declare -a tree=(bin doc etc lib test vendor)
+
+  for dir in ${tree[@]}; do
+    echo -n "Checking $project_path/$dir ... "
+    if [[ -d $project_path/$dir ]]; then
+      echo "Exists"
+      result=(${result[@]} 0)
+    else
+      echo "Not exists"
+      result=(${result[@]} 1)
+    fi
+    success=(${success[@]} 0)
+  done
+
+  [[ ${result[*]} == ${success[*]} ]] && echo "R: Success" || echo "R: Failure"
+}
+
+declare -a test_list=()
+
+for f in $(declare -F); do
+  [[ $f =~ 'test' ]] && t=${f/'declare'} || t=''
+  test_list=(${test_list[@]} ${t/'-f'})
+done
+
+for t in ${test_list[@]}; do
+  echo "T: Running ${t} ..."
+  $t
+done
+
+exit $?
